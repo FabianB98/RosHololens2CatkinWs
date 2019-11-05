2418b25ec80269c1d4e31d0019663e35820c0b91
Fabian Bösing <fabian_boesing@arcor.de>
2019-11-05 16:14:18 +0100

    Point cloud is now being transformed from camera space to world space.

--- src/hololens_point_cloud_receiver/src/Main.cpp
+++ src/hololens_point_cloud_receiver/src/Main.cpp
@@ -2,19 +2,23 @@
 
 int main(int argc, char **argv)
 {
+    // Initialize everything.
     ros::init(argc, argv, "PointCloudListener");
 
     ros::NodeHandle n;
 
-    ros::Subscriber shortThrowDepthSubscriber = n.subscribe(SHORT_THROW_DEPTH_TOPIC, 1000, shortThrowDepthFrameCallback);
-    ros::Subscriber longThrowDepthSubscriber = n.subscribe(LONG_THROW_DEPTH_TOPIC, 1000, longThrowDepthFrameCallback);
-    ros::Subscriber shortThrowDirectionsSubscriber = n.subscribe(SHORT_THROW_PIXEL_DIRECTIONS_TOPIC, 1000, shortThrowPixelDirectionsCallback);
-    ros::Subscriber longThrowDirectionsSubscriber = n.subscribe(LONG_THROW_PIXEL_DIRECTIONS_TOPIC, 1000, longThrowPixelDirectionsCallback);
+    ros::Subscriber shortThrowDepthSubscriber = n.subscribe(SHORT_THROW_DEPTH_TOPIC, 10, shortThrowDepthFrameCallback);
+    ros::Subscriber longThrowDepthSubscriber = n.subscribe(LONG_THROW_DEPTH_TOPIC, 10, longThrowDepthFrameCallback);
+    ros::Subscriber shortThrowDirectionsSubscriber = n.subscribe(SHORT_THROW_PIXEL_DIRECTIONS_TOPIC, 10, shortThrowPixelDirectionsCallback);
+    ros::Subscriber longThrowDirectionsSubscriber = n.subscribe(LONG_THROW_PIXEL_DIRECTIONS_TOPIC, 10, longThrowPixelDirectionsCallback);
 
     pointCloudReceiver = new PointCloudReceiver(n);
 
-    ros::spin();
+    // Perform the update loop using a multi threaded spinner with an amount of threads equal to the core count of the CPU.
+    ros::MultiThreadedSpinner spinner(0);
+    spinner.spin();
 
+    // Clean up.
     delete pointCloudReceiver;
 
     return 0;
--- src/hololens_point_cloud_receiver/src/PointCloudReceiver.cpp
+++ src/hololens_point_cloud_receiver/src/PointCloudReceiver.cpp
@@ -7,15 +7,15 @@ PointCloudReceiver::PointCloudReceiver(ros::NodeHandle n)
 {
     ROS_INFO("Creating PointCloudReceiver...");
 
-    shortThrowImagePublisher = n.advertise<sensor_msgs::Image>(SHORT_THROW_IMAGE_TOPIC, 1000);
-    longThrowImagePublisher = n.advertise<sensor_msgs::Image>(LONG_THROW_IMAGE_TOPIC, 1000);
+    shortThrowImagePublisher = n.advertise<sensor_msgs::Image>(SHORT_THROW_IMAGE_TOPIC, 10);
+    longThrowImagePublisher = n.advertise<sensor_msgs::Image>(LONG_THROW_IMAGE_TOPIC, 10);
 }
 
 void PointCloudReceiver::handleShortThrowDepthFrame(const hololens_point_cloud_msgs::DepthFrame::ConstPtr& msg)
 {
     ROS_INFO("Received a short throw depth frame!");
     handleDepthFrame(msg, shortThrowDirections, SHORT_THROW_MIN_RELIABLE_DEPTH, SHORT_THROW_MAX_RELIABLE_DEPTH,
-            shortThrowImagePublisher, &shortThrowSequenceNumber, "");
+            shortThrowImagePublisher, &shortThrowSequenceNumber, "short_throw_point_cloud");
 }
 
 void PointCloudReceiver::handleLongThrowDepthFrame(const hololens_point_cloud_msgs::DepthFrame::ConstPtr& msg)
@@ -65,8 +65,8 @@ void PointCloudReceiver::handleDepthFrame(
             image.data.push_back(static_cast<uint8_t>(255.0f * depthMap.valueAt(u, v) / (maxReliableDepth * 1000.0f)));
     imagePublisher.publish(image);
 
-    // Calculate the point cloud.
-    pcl::PointCloud<pcl::PointXYZ>::Ptr pointCloud (new pcl::PointCloud<pcl::PointXYZ>());
+    // Calculate the point cloud (in camera space).
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pointCloudCamSpace (new pcl::PointCloud<pcl::PointXYZ>());
     for (uint32_t i = 0; i < pixelDirections->pixelDirections.size(); ++i)
     {
         // Get the depth at the current pixel.
@@ -77,10 +77,35 @@ void PointCloudReceiver::handleDepthFrame(
             continue;
 
         // Calculate the point for the current pixel based on the pixels depth value.
-        pointCloud->push_back(pcl::PointXYZ(-dir.direction.x * depth, -dir.direction.y * depth, -dir.direction.z * depth));
+        pointCloudCamSpace->push_back(pcl::PointXYZ(-dir.direction.x * depth, -dir.direction.y * depth, -dir.direction.z * depth));
     }
 
-    //Visualize the point cloud.
+    // Transform the point cloud from camera space to world space.
+    Eigen::Matrix4f transform = Eigen::Matrix4f::Identity();
+    transform(0, 0) = depthFrame->camToWorld.m11;
+    transform(1, 0) = depthFrame->camToWorld.m12;
+    transform(2, 0) = depthFrame->camToWorld.m13;
+    transform(3, 0) = depthFrame->camToWorld.m14;
+
+    transform(0, 1) = depthFrame->camToWorld.m21;
+    transform(1, 1) = depthFrame->camToWorld.m22;
+    transform(2, 1) = depthFrame->camToWorld.m23;
+    transform(3, 1) = depthFrame->camToWorld.m24;
+
+    transform(0, 2) = depthFrame->camToWorld.m31;
+    transform(1, 2) = depthFrame->camToWorld.m32;
+    transform(2, 2) = depthFrame->camToWorld.m33;
+    transform(3, 2) = depthFrame->camToWorld.m34;
+
+    transform(0, 3) = depthFrame->camToWorld.m41;
+    transform(1, 3) = depthFrame->camToWorld.m42;
+    transform(2, 3) = depthFrame->camToWorld.m43;
+    transform(3, 3) = depthFrame->camToWorld.m44;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pointCloudWorldSpace (new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::transformPointCloud(*pointCloudCamSpace, *pointCloudWorldSpace, transform);
+
+    // Visualize the point cloud.
     if (pointCloudName.size() > 0)
-        cloudViewer.showCloud(pointCloud, pointCloudName);
+        cloudViewer.showCloud(pointCloudWorldSpace, pointCloudName);
 }
