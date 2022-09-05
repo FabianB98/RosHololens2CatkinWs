c29d1a7bf327e77860f07a4828fc89441d5d98b9
Fabian Bösing <fabian_boesing@arcor.de>
2022-09-05 15:23:13 +0200

    Modified tracking_accuracy_visualizer to also visualize the trajectories and the error between the object detections and the external tracking system.

--- src/tracking_accuracy_test/src/tracking_accuracy_visualizer.cpp
+++ src/tracking_accuracy_test/src/tracking_accuracy_visualizer.cpp
@@ -19,6 +19,7 @@
 #include <pcl/point_types.h>
 #include <pcl/common/common.h>
 #include <pcl/common/centroid.h>
+#include <pcl/common/distances.h>
 #include <pcl/common/transforms.h>
 #include <pcl/registration/transformation_estimation_2D.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -33,6 +34,9 @@ ros::Publisher stationaryTrackingSystemPublisher;
 ros::Publisher stationaryObjectDetectionsPublisher;
 ros::Publisher movingTrackingSystemPublisher;
 ros::Publisher movingObjectDetectionsPublisher;
+ros::Publisher trajectoryTrackingSystemPublisher;
+ros::Publisher trajectoryObjectDetectionsPublisher;
+ros::Publisher trajectoryDifferencesPublisher;
 
 int main(int argc, char** argv) { 
     ros::init(argc, argv, "tracking_accuracy_visualizer");
@@ -53,6 +57,9 @@ int main(int argc, char** argv) {
     stationaryObjectDetectionsPublisher = publicNodeHandle.advertise<sensor_msgs::PointCloud2>("stationaryObjectDetections", 100);
     movingTrackingSystemPublisher = publicNodeHandle.advertise<sensor_msgs::PointCloud2>("movingTrackingSystem", 100);
     movingObjectDetectionsPublisher = publicNodeHandle.advertise<sensor_msgs::PointCloud2>("movingObjectDetections", 100);
+    trajectoryTrackingSystemPublisher = publicNodeHandle.advertise<visualization_msgs::MarkerArray>("trajectoryTrackingSystem", 100);
+    trajectoryObjectDetectionsPublisher = publicNodeHandle.advertise<visualization_msgs::MarkerArray>("trajectoryObjectDetections", 100);
+    trajectoryDifferencesPublisher = publicNodeHandle.advertise<visualization_msgs::MarkerArray>("trajectoryDifferences", 100);
 
     std::string home = std::string(getenv("HOME"));
     std::string directory = home + resultsPathPrefixRelativeToHome;
@@ -151,6 +158,89 @@ int main(int argc, char** argv) {
     movingObjectDetectionsMsg.header.stamp = ros::Time::now();
     movingObjectDetectionsMsg.header.frame_id = "map";
 
+    visualization_msgs::MarkerArray trajectoryTrackingSystemMarkerArray;
+    visualization_msgs::Marker trajectoryTrackingSystemMarker;
+    trajectoryTrackingSystemMarker.header.stamp = ros::Time::now();
+    trajectoryTrackingSystemMarker.header.frame_id = "map";
+    trajectoryTrackingSystemMarker.ns = "trajectory";
+    trajectoryTrackingSystemMarker.id = 0;
+    trajectoryTrackingSystemMarker.type = visualization_msgs::Marker::LINE_STRIP;
+    for (size_t i = 0; i < allTrackingSystemCloud.points.size(); i++) {
+        geometry_msgs::Point point;
+        point.x = allTrackingSystemCloud.points[i].x;
+        point.y = allTrackingSystemCloud.points[i].y;
+        point.z = allTrackingSystemCloud.points[i].z;
+        trajectoryTrackingSystemMarker.points.push_back(point);
+    }
+    trajectoryTrackingSystemMarker.scale.x = 0.005;
+    trajectoryTrackingSystemMarker.color.r = 1.0;
+    trajectoryTrackingSystemMarker.color.g = 0.5;
+    trajectoryTrackingSystemMarker.color.b = 0.0;
+    trajectoryTrackingSystemMarker.color.a = 1.0;
+    trajectoryTrackingSystemMarker.lifetime = ros::Duration(0.25);
+    trajectoryTrackingSystemMarkerArray.markers.push_back(trajectoryTrackingSystemMarker);
+
+    visualization_msgs::MarkerArray trajectoryObjectDetectionsMarkerArray;
+    visualization_msgs::Marker trajectoryObjectDetectionsMarker;
+    trajectoryObjectDetectionsMarker.header.stamp = ros::Time::now();
+    trajectoryObjectDetectionsMarker.header.frame_id = "map";
+    trajectoryObjectDetectionsMarker.ns = "trajectory";
+    trajectoryObjectDetectionsMarker.id = 0;
+    trajectoryObjectDetectionsMarker.type = visualization_msgs::Marker::LINE_STRIP;
+    for (size_t i = 0; i < allObjectDetectionsCloud.points.size(); i++) {
+        geometry_msgs::Point point;
+        point.x = allObjectDetectionsCloud.points[i].x;
+        point.y = allObjectDetectionsCloud.points[i].y;
+        point.z = allObjectDetectionsCloud.points[i].z;
+        trajectoryObjectDetectionsMarker.points.push_back(point);
+    }
+    trajectoryObjectDetectionsMarker.scale.x = 0.005;
+    trajectoryObjectDetectionsMarker.color.r = 0.0;
+    trajectoryObjectDetectionsMarker.color.g = 1.0;
+    trajectoryObjectDetectionsMarker.color.b = 1.0;
+    trajectoryObjectDetectionsMarker.color.a = 1.0;
+    trajectoryObjectDetectionsMarker.lifetime = ros::Duration(0.25);
+    trajectoryObjectDetectionsMarkerArray.markers.push_back(trajectoryObjectDetectionsMarker);
+
+    visualization_msgs::MarkerArray trajectoryDifferencesMarkerArray;
+    visualization_msgs::Marker trajectoryDifferencesMarker;
+    trajectoryDifferencesMarker.header.stamp = ros::Time::now();
+    trajectoryDifferencesMarker.header.frame_id = "map";
+    trajectoryDifferencesMarker.ns = "trajectoryDifference";
+    trajectoryDifferencesMarker.id = 0;
+    trajectoryDifferencesMarker.type = visualization_msgs::Marker::LINE_LIST;
+    float maxDistance = 0.0;
+    for (size_t i = 0; i < allTrackingSystemCloud.points.size(); i++) {
+        float distance = pcl::euclideanDistance(allTrackingSystemCloud.points[i], allObjectDetectionsCloud.points[i]);
+        maxDistance = std::max(maxDistance, distance);
+    }
+    for (size_t i = 0; i < allTrackingSystemCloud.points.size(); i++) {
+        geometry_msgs::Point trackingSystemPoint;
+        trackingSystemPoint.x = allTrackingSystemCloud.points[i].x;
+        trackingSystemPoint.y = allTrackingSystemCloud.points[i].y;
+        trackingSystemPoint.z = allTrackingSystemCloud.points[i].z;
+        trajectoryDifferencesMarker.points.push_back(trackingSystemPoint);
+
+        geometry_msgs::Point objectDetectionsPoint;
+        objectDetectionsPoint.x = allObjectDetectionsCloud.points[i].x;
+        objectDetectionsPoint.y = allObjectDetectionsCloud.points[i].y;
+        objectDetectionsPoint.z = allObjectDetectionsCloud.points[i].z;
+        trajectoryDifferencesMarker.points.push_back(objectDetectionsPoint);
+
+        float distance = pcl::euclideanDistance(allTrackingSystemCloud.points[i], allObjectDetectionsCloud.points[i]);
+        float distanceRelativeToMaxDistance = distance / maxDistance;
+        std_msgs::ColorRGBA lineColor;
+        lineColor.r = distanceRelativeToMaxDistance;
+        lineColor.g = 1.0 - distanceRelativeToMaxDistance;
+        lineColor.b = 0.0;
+        lineColor.a = 1.0;
+        trajectoryDifferencesMarker.colors.push_back(lineColor);
+        trajectoryDifferencesMarker.colors.push_back(lineColor);
+    }
+    trajectoryDifferencesMarker.scale.x = 0.0025;
+    trajectoryDifferencesMarker.lifetime = ros::Duration(0.25);
+    trajectoryDifferencesMarkerArray.markers.push_back(trajectoryDifferencesMarker);
+
     ros::Rate rate(10);
     while (ros::ok()) {
         allTrackingSystemPublisher.publish(allTrackingSystemMsg);
@@ -161,6 +251,9 @@ int main(int argc, char** argv) {
         stationaryObjectDetectionsPublisher.publish(stationaryObjectDetectionsMsg);
         movingTrackingSystemPublisher.publish(movingTrackingSystemMsg);
         movingObjectDetectionsPublisher.publish(movingObjectDetectionsMsg);
+        trajectoryTrackingSystemPublisher.publish(trajectoryTrackingSystemMarkerArray);
+        trajectoryObjectDetectionsPublisher.publish(trajectoryObjectDetectionsMarkerArray);
+        trajectoryDifferencesPublisher.publish(trajectoryDifferencesMarkerArray);
 
         rate.sleep();
     }
